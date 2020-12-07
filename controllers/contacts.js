04252d271233166687dbbdff7e2b19349f658bf8
Nilesh D <nileshlund@gmail.com>
2020-12-07 21:33:54 +0530

    feat: Create async await middleware

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,68 +1,53 @@
 const Contact = require('../models/Contact');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc       Get all contacts
 // @route      GET /contacts
 // @access     Private
-const getContacts = async (req, res, next) => {
-  try {
-    const contacts = await Contact.find();
+const getContacts = asyncHandler(async (req, res, next) => {
+  const contacts = await Contact.find();
 
-    res.status(200).json({ success: true, count: contacts.length, data: contacts });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, count: contacts.length, data: contacts });
+});
 
 // @desc       Create new contact
 // @route      POST /contacts
 // @access     Private
-const createContact = async (req, res, next) => {
-  try {
-    const contact = await Contact.create(req.body);
+const createContact = asyncHandler(async (req, res, next) => {
+  const contact = await Contact.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: contact,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: contact,
+  });
+});
 
 // @desc       Update contact
 // @route      PUT /contacts/:id
 // @access     Private
-const updateContact = async (req, res, next) => {
-  try {
-    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+const updateContact = asyncHandler(async (req, res, next) => {
+  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
-    if (!contact) {
-      return next(new ErrorResponse(`Contact with the ID of ${req.params.id} does not exist.`, 404));
-    }
-
-    res.status(200).json({ success: true, data: contact });
-  } catch (err) {
-    next(err);
+  if (!contact) {
+    return next(new ErrorResponse(`Contact with the ID of ${req.params.id} does not exist.`, 404));
   }
-};
+
+  res.status(200).json({ success: true, data: contact });
+});
 
 // @desc       Delete contact
 // @route      DELETE /contacts/:id
 // @access     Private
-const deleteContact = async (req, res, next) => {
-  try {
-    const contact = await Contact.findByIdAndDelete(req.params.id);
+const deleteContact = asyncHandler(async (req, res, next) => {
+  const contact = await Contact.findByIdAndDelete(req.params.id);
 
-    if (!contact) {
-      return next(new ErrorResponse(`Contact with the ID of ${req.params.id} does not exist.`, 404));
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    next(err);
+  if (!contact) {
+    return next(new ErrorResponse(`Contact with the ID of ${req.params.id} does not exist.`, 404));
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
 
 module.exports = {
   getContacts,
