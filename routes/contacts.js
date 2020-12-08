37fa52a04f991b14ee21b30f10af031c04e674b3
Nilesh D <nileshlund@gmail.com>
2020-12-08 07:26:31 +0530

    feat: Pass auth middleware to check if user is authenticated to access routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { createContact, getContacts, updateContact, deleteContact } = require('../controllers/contacts');
 
+const { protect } = require('../middleware/auth');
+
 const router = express.Router();
 
-router.route('/').get(getContacts).post(createContact);
+router.route('/').get(protect, getContacts).post(protect, createContact);
 
-router.route('/:id').put(updateContact).delete(deleteContact);
+router.route('/:id').put(protect, updateContact).delete(protect, deleteContact);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { renderRegister, registerUser, renderLogin, loginUser } = require('../controllers/users');
+const { renderRegister, registerUser, renderLogin, loginUser, getMe } = require('../controllers/users');
 
 const router = express.Router();
+
+const { protect } = require('../middleware/auth');
 
 router.route('/register').get(renderRegister).post(registerUser);
 
 router.route('/login').get(renderLogin).post(loginUser);
+
+router.route('/me').get(protect, getMe);
 
 module.exports = router;
