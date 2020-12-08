26962fda55e105f07a9d806ee9f5d28272db7572
Nilesh D <nileshlund@gmail.com>
2020-12-08 07:33:54 +0530

    feat: Add role authorization for protected routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -28,4 +28,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { protect };
+// Grant access to specific roles
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 403));
+    }
+    next();
+  };
+};
+
+module.exports = { protect, authorize };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { createContact, getContacts, updateContact, deleteContact } = require('../controllers/contacts');
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/').get(protect, getContacts).post(protect, createContact);
+router.route('/').get(protect, getContacts).post(protect, authorize('Member', 'Admin'), createContact);
 
-router.route('/:id').put(protect, updateContact).delete(protect, deleteContact);
+router
+  .route('/:id')
+  .put(protect, authorize('Member', 'Admin'), updateContact)
+  .delete(protect, authorize('Member', 'Admin'), deleteContact);
 
 module.exports = router;
