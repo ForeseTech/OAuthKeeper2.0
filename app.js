af0bb5b96426152ea5b4f42b8bc17e0ea8b610b6
Nilesh D <nileshlund@gmail.com>
2020-12-11 11:13:45 +0530

    feat: Middleware for method-override, express-session and flash

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
 const colors = require('colors');
 const ejsMate = require('ejs-mate');
 
@@ -28,8 +31,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Method-override middleware
+app.use(
+  methodOverride((req, res) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 // Serve static assets
 app.use(express.static(path.join(__dirname, 'public')));
+
+// express-session middleware
+const sessionConfig = {
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+  // store: new MongoStore({ mongooseConnection: mongoose.connection }),
+};
+app.use(session(sessionConfig));
+
+// connect-flash middleware
+app.use(flash());
+
+// flash middleware
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+
+  next();
+});
 
 // Cookie parser middleware
 app.use(cookieParser());
