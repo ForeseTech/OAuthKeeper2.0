2a490a3524bf43ea3e754731877bb1275e9b8379
Nilesh D <nileshlund@gmail.com>
2020-12-22 13:43:30 +0530

    fix: Minor bug fix

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV.underline} mode on port ${PORT}.`.yellow.bold);
 });
 
