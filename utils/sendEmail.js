8f72d1300ef774c893aea35f21ea56d4855ecf7a
Nilesh D <nileshlund@gmail.com>
2020-12-24 09:22:22 +0530

    fix: Use SendGrid API to send reset password link

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,16 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
-
-  // Send mail with defined transport object
-  const message = {
-    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+  const msg = {
     to: options.email,
+    from: process.env.FROM_EMAIL,
     subject: options.subject,
-    text: options.message,
+    html: options.message,
   };
 
-  const info = await transporter.sendMail(message);
-
-  console.log(info.messageId);
+  await sgMail.send(msg);
 };
 
 module.exports = sendEmail;
