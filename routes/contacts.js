f26d31f45d0a386c87e76e66b1f84f67397940fc
Nilesh D <nileshlund@gmail.com>
2020-12-30 11:58:17 +0530

    fix: Add route to view tabulated data

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const { createContact, getContacts, updateContact, deleteContact, getStatistics } = require('../controllers/contacts');
+const {
+  createContact,
+  getContacts,
+  updateContact,
+  deleteContact,
+  getStatistics,
+  getData,
+} = require('../controllers/contacts');
 
 const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/').get(protect, getContacts).post(protect, authorize('Member', 'Admin'), createContact);
+router.route('/panel').get(protect, getData);
 router.route('/statistics').get(protect, getStatistics);
 
 router
